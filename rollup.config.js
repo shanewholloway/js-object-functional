8eed22ad8a7e4110f38d44070ebc5d46383adcf9
Shane Holloway <shane.holloway@ieee.org>
2018-02-20 14:46:10 -0700

    Updated rollup packaging to use cjs/ esm/ and umd/ directories

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
 import pkg from './package.json'
 import {minify} from 'uglify-es'
 import rpi_uglify from 'rollup-plugin-uglify'
-import rpi_babel from 'rollup-plugin-babel'
+import rpi_jsy from 'rollup-plugin-jsy-babel'
 
 const sourcemap = 'inline'
 
-const plugins = [jsy_plugin()]
+const plugins = [rpi_jsy()]
 const ugly = { compress: {warnings: false}, output: {comments: false}, sourceMap: false }
 const prod_plugins = plugins.concat([rpi_uglify(ugly, minify)])
 
 const core = [
-	{ input: 'code/index.jsy',
-		output: [
-      { file: pkg.main, format: 'cjs', exports:'named' },
-      { file: pkg.module, format: 'es' }],
-    sourcemap, external:[], plugins },
+  { input: 'code/index.jsy',
+    output: [
+      { file: pkg.module, format: 'es', sourcemap },
+      { file: pkg.main, format: 'cjs', exports:'named', sourcemap },
+      { file: 'umd/object-functional.js', format: 'umd', name: 'object-functional', exports:'named' },
+    ],
+    external:[], plugins },
 
-	{ input: 'code/index.jsy',
-		output: { file: pkg.browser, format: 'amd', exports:'named' },
-    external:[], plugins: prod_plugins },
+  prod_plugins &&
+    { input: 'code/index.jsy',
+      output: { file: pkg.browser, format: 'umd', name: 'object-functional', exports:'named' },
+      external:[], plugins: prod_plugins },
 ]
 
 const adapters = [
@@ -29,23 +32,11 @@ const adapters = [
   {name: 'json'},
   {name: 'seamless-immutable'},
 ].map(({name, external}) => (
-	{ input: `code/${name}.js`,
-		output: [
-      { file: `dist/${name}.js`, format: 'cjs' },
-      { file: `dist/${name}.mjs`, format: 'es' }],
-    sourcemap, external:external || [name], plugins }))
+  { input: `code/${name}.js`,
+    output: [
+      { file: `cjs/${name}.js`, format: 'cjs', sourcemap },
+      { file: `esm/${name}.js`, format: 'es', sourcemap }],
+    external:external || [name], plugins }))
 
 
 export default [].concat(core, adapters)
-
-
-function jsy_plugin() {
-  const jsy_preset = [ 'jsy/lean', {
-    no_stage_3: true, modules: false,
-    targets: { node: "7.1", browsers: "> 5%" }} ]
-  return rpi_babel({
-    exclude: 'node_modules/**',
-    presets: [ jsy_preset ],
-    plugins: [],
-    babelrc: false }) }
-
